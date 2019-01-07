f7aa7851aa8ffe8e7ba2ab6fe6f5a5d7f7ceb132
yujie zhu <yujiezhu718@gmail.com>
2019-01-07 13:37:17 -0800

    configured DB settings

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'crmapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crmeasydb',
+        'USER': 'yujiezhu',
+        'PASSWORD': 'password',
+        'HOST': '/tmp',
+        'PORT': '5432',
     }
 }
 
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yujiezhu/tutorials/crmeasy/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
